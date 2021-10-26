4625c38e5a114b85cc79b254f1f515cb7712662f
nishuai03 <nishuai03@kuaishou.com>
2021-10-26 15:56:51 +0800

    feat: 添加node版本检查

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,12 +2,32 @@
 
 module.exports = core;
 
-const pkg = require('../package.json')
+const semver = require('semver')
+const colors = require('colors')
 const log = require('@ns-cli-dev/log')
 
+const pkg = require('../package.json')
+const constant = require('./const')
+
 function core() {
-    console.log('lib/index core方法执行了')
-    checkPackageVersion()
+    try {
+        checkPackageVersion()
+        checkNodeVersion()
+    } catch (e) {
+        log.error(e.message)
+    }
+    
+}
+
+function checkNodeVersion() {
+    const curVersion = process.version
+    const lowestVersion = constant.LOWEST_NODE_VERSION
+    console.log(process.version)
+    console.log(constant.LOWEST_NODE_VERSION)
+    if(!semver.gte(curVersion, lowestVersion)){
+        throw new Error(colors.red(`需要node版本再${lowestVersion}以上`))
+    }
+
 }
 
 function checkPackageVersion() {
